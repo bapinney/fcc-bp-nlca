cabc9747dc173a281d2f44a1d136b312440125d6
Branden Pinney <bapinney@gmail.com>
2016-06-30 18:57:58 -0700

    Enhancements to search, geolocation, et. al.

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,5 +1,6 @@
 angular.module("fcc-bp-nlca", ['ui.router']);
 
+var currentPos;
 $(function(){
     $('[data-toggle="tooltip"]').tooltip();
     if ("geolocation" in navigator) {
@@ -9,6 +10,18 @@ $(function(){
     $("#get-location").click(function() {
         navigator.geolocation.getCurrentPosition(function(position) {
             console.log(position.coords.latitude, position.coords.longitude);
+            currentPos = position.coords.latitude.toFixed(5) + ", " + position.coords.longitude.toFixed(5);
+            console.log(currentPos);
+            $("#search-box")[0].value = currentPos;
+            $("#search-box").trigger("input");
         }); 
     });
+    $("#search-box")[0].oninput = function() {
+        if($("#search-box")[0].value.length > 0 && $("#search-button")[0].disabled) {
+            $("#search-button")[0].disabled = false;
+        }
+        if($("#search-box")[0].value.length == 0 && !$("#search-button")[0].disabled) {
+            $("#search-button")[0].disabled = true;
+        }
+    };
 });
