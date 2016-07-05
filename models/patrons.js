fa299c6f0eb090faf0febfa0eb8b166f660cd872
Branden Pinney <bapinney@gmail.com>
2016-07-05 10:44:32 -0700

    Fixed the problem mentioned in the last commit comment
    It was saving in fccnlca-users instead of fccnlca-patrons collection

--- models/patrons.js
+++ models/patrons.js
@@ -11,7 +11,7 @@ var patronSchema = new Schema({
     listingId   : String,
     patrons     : [UserSchema], //Allows us to add an array of Users (i.e. patrons)
     },
-    {collection: 'fccnlca-users'}
+    {collection: 'fccnlca-patrons'}
 );
 
 /*  We are allowed to write methods to Schemas
